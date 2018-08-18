4b758fb51f82e5fb2e4b9bb155a6ccfc26fdbc35
Alyson Takai <shtakai@gmail.com>
2018-08-18 12:29:59 +0700

    Modify attributes on business object

--- app/models/business.rb
+++ app/models/business.rb
@@ -5,13 +5,13 @@ class Business
     @id = Faker::Number.number(12)
     @name = Faker::Food.dish
     @category = Faker::Food.ingredient
-    @address = Faker::Address.street_address
+    @address1 = Faker::Address.street_address
     @city = Faker::Address.city
     @state = Faker::Address.state
     @zip_code = Faker::Address.zip
     @rating = rand(5) + 1
     @review_counts = rand(1000) + 50
-    @image_src = 'https://picsum.photos/200/300/?random'
+    @image_src = "https://loremflickr.com/320/240?t=#{@id}"
   end
 
 end
